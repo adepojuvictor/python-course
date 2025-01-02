d0ff882f496949e99cdcd72f6856d3515a928a99
Adepoju Victor <adepojuvictor2705@gmail.com>
2025-01-02 14:18:19 +0100

    i created victor.py for range function

---
+++ victor.py
@@ -0,0 +1,15 @@
+f= 5.6
+name="victor"
+age=36
+print(name)
+print(age)
+birth_year=input("Enter your birth year")
+age = 2024 - int(birth_year)
+print(age)
+print(name) 
+name ="Gbenga"
+print(name)
+name ="gbenga"
+age=56
+print(name)
+print(age)
